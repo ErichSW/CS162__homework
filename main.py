1949f4d9d923e527c74f7a9a9cc99d6ced2e20e2
Erich Winchester <erichswinches@gmail.com>
2024-04-11 16:01:54 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plot
+
+# set up your lists
+
+numlist = [10, 10, 2, 4]
+namelist = ['female', 'male', 'hermaphrodite', 'asexual']
+colorlist = ['blue', 'red', 'purple', 'green']
+explodelist = [0.0, 0.0, 0.2, 0.0]
+
+# make the pie chart
+
+plot.pie(numlist, labels=namelist, autopct='%.1f%%', colors=colorlist,
+         explode=explodelist, startangle=75)
+plot.axis('equal')
+plot.savefig('piechart.png')
